d9347a0247efa7e0e6510b6e7b4af0684e683abc
Alejo2r <sadith.alejandro@gmail.com>
2025-08-01 14:21:29 -0600

    Ejercicio un completado, e inicio de ejercicio 2 llenando el arreglo segun la formula

--- Lab2P3_SadithRamos.cpp
+++ Lab2P3_SadithRamos.cpp
@@ -1,19 +1,108 @@
-// Lab2P3_SadithRamos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+using namespace std;
+#include <array>
+void printArray(float arreglo[]) {
+    for (int indice = 0; indice < 4; indice++){
+        cout << "[" << arreglo[indice] << "]" << endl;
+    }
+}
+void descendente(float arreglo[], int size) {
+    for (int i = 0; i < size; ++i)
+        for (int j = 0; j < size - i - 1; ++j) {
+            float tempo = arreglo[j];
+            if (tempo < arreglo[j + 1]) {
+                arreglo[j] = arreglo[j + 1];
+                arreglo[j + 1] = tempo;
+            }
+        }
+    printArray(arreglo);
+}
+void ranking(float arreglo[], int size) {
+    for (int i = 0; i < size; ++i)
+        for (int j = 0; j < size - i - 1; ++j) {
+            float tempo = arreglo[j];
+            if (tempo > arreglo[j + 1]) {
+                arreglo[j] = arreglo[j + 1];
+                arreglo[j + 1] = tempo;
+            }
+        }
+    for (int indice = 0; indice < 4; indice++) {
+        cout << "#" << indice+1 << ".   " << "[" << arreglo[indice] << "]" << endl;
+    }
+}
+int main(){
+    int op;
+    float temporal = 0;
+    do {
+        cout << "1. Ejercicio #1" << endl;
+        cout << "2. Ejercicio #2" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Ingrese la opcion de desee" << endl;
+        cin >> op;
+        switch (op) {
+        case 1:
+            int opc;
+            do {
+                cout << "Bienvenido al sistema de Time Trials" << endl;
+                cout << "1. Ingresar tiempos" << endl;
+                cout << "2. Tiempos descendentes" << endl;
+                cout << "3. Ranking de Tiempos" << endl;
+                cout << "4. Salir" << endl;
+                cin >> opc;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+                switch (opc) {
+                    float arreglo[4];
+                case 1:
+                    for (int indice = 0; indice < 4; indice++){
+                        cout << "Ingrese el tiempo: ";
+                        cin >> arreglo[indice];
+                    }
+                    cout << "Tiempos registrados!" << endl;
+                    break;
+                case 2:
+                    if (arreglo == 0) {
+                        cout << "Registra tus tiempos antes!" << endl;
+                    }
+                    else {
+                        descendente(arreglo, 4);
+                    }
+                    break;
+                case 3:
+                    if (arreglo == 0) {
+                        cout << "Registra tus tiempos antes!" << endl;
+                    }
+                    else {
+                        ranking(arreglo, 4);
+                    }
+                    break;
+                }
+            } while(opc != 4);
+            break;
+        case 2:
+            int num;
+            float arreglo[10];
+            for (int i = 0; i < 10; i++){
+                arreglo[i] = temporal + sqrt(i + 1) + pow(-1,i) * log(i+2);
+                temporal = arreglo[i];
+            }
+            do{
+                for (int i = 0; i < 10; i++) {
+                    cout << arreglo[i] << endl;
+                }
+                cout << "Menu Sumatoria" << endl;
+                cout << "1. " << endl;
+                cout << "2. Tiempos descendentes" << endl;
+                cout << "3. Ranking de Tiempos" << endl;
+                cout << "4. Salir" << endl;
+                cin >> num;
+            } while (num!=4);
+            break;
+        case 3:
+            cout << "Gracias por usar el programa";
+            break;
+        default:
+            cout << "Opcion invalida" << endl;
+        }
+    } while (op != 3);
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
